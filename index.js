1d76f3ed3ef257bb6b4a933f27b08befbd854863
sadiajuthi <sadia.5343@gmail.com>
2022-05-27 12:41:38 +0530

    mongodb coencted

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// connect to mongoDB
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wujqyvj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB connected');
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir)
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
+
 
 app.get('/', (req, res) => {
     res.send('Hello from, tradebd')
