05adb682e5b8b9f121f3f7f34ef06146dfaf95e0
sadiajuthi <sadia.5343@gmail.com>
2022-05-28 17:38:09 +0530

    admin role added in user

--- index.js
+++ index.js
@@ -36,8 +36,24 @@ async function run() {
             res.send(users);
         });
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
 
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
 
+            const filter = { email: email };
+
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
